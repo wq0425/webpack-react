a0e79660d91923540ba73415e324e64233dcacac
wangqin <wangqin@163.com>
2021-09-12 10:24:59 +0800

    增加README文档

---
+++ README.md
@@ -0,0 +1,5 @@
+1.`npm i`安装包
+
+2.`npm start`启动项目
+
+注释：若只有`antd`换肤生效，请再单独安装`antd-theme-generator@1.1.7`包，启动项目便可生效
--- src/index.js
+++ src/index.js
@@ -4,8 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.less';
 import App from './App';
 
-// import 'antd/dist/antd.less';
-// // import reportWebVitals from './reportWebVitals';
+// import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -14,7 +13,4 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 // reportWebVitals();
